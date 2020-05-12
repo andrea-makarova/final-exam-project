723ba6501c5d6353082ba95aac5c22b1e1287f08
Andrea Makarová <andreamakarova@MacBook-Air-uzivatela-Andrea.local>
2020-05-12 14:09:30 +0200

    fetching on homepage working

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ menu.addEventListener("click", openNav);
 function openNav() {
   if (navigation.style.display === "block") {
     navigation.style.display = "none";
+
   } else {
     navigation.style.display = "block";
   }
@@ -54,3 +55,43 @@ function closeSmallNav() {
 
 };
 
+let Actualities = "https://andreamakarova.com/wordpress/wp-json/wp/v2/aktualita?_embed&per_page=20";
+const template1 = document.querySelector("#template1").content;
+const parent1 = document.querySelector("#main1");
+
+
+
+function loadData() {
+    fetch(Actualities).then(e => e.json()).then(show);
+}
+
+function show(data) {
+    data.forEach(post => {
+
+console.log(post);
+
+        //clone
+        const clone1 = template1.cloneNode(true);
+        //populate
+        const img1 = clone1.querySelector("#img1");
+        const title = clone1.querySelector("h1");
+        const description = clone1.querySelector("p");
+
+
+        img1.src = post._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+
+
+        title.textContent = post.title.rendered;
+        description.textContent = post.short_description;
+
+
+        console.log(post);
+
+
+
+        //append
+        parent1.appendChild(clone1);
+    });
+}
+
+loadData(Actualities);
