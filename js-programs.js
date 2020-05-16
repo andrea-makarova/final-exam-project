7b2c658d59188bb6cb967178c970a6b54a3581ed
andrea-makarova <andreamakarova99@gmail.com>
2020-05-16 10:30:33 +0200

    udalosti done

--- js-programs.js
+++ js-programs.js
@@ -426,15 +426,3 @@ function openPd1() {
 
 };
 
-//opens specific program
-if (window.location.hash === '#1id') {
-    openInfo1();
-  }
-
-if (window.location.hash === '#2id') {
-    openInfo2();
-  }
-
-if (window.location.hash === '#3id') {
-    openInfo3();
-  }
